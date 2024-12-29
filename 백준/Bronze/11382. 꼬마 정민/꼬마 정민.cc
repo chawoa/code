9e2f04594975054c6ea83c70294ed56533106e94
chawoa <129825032+chawoa@users.noreply.github.com>
2024-12-29 20:10:24 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    long long x, y, z, result;
+    cin>> x >> y >> z;
+
+    result = x + y + z;
+    cout<<result<<endl;
+}
