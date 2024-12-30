6a9c802f683ffc588b5a40b1207185f617e9bde3
chawoa <129825032+chawoa@users.noreply.github.com>
2024-12-30 23:40:45 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    short n;
+    cin>>n;
+    if (n >= 90){
+        cout<<"A"<<endl;
+    }
+    else if (n < 90 && n >= 80){
+        cout<<"B"<<endl;
+    }
+    else if (n < 80 && n >= 70){
+        cout<<"C"<<endl;
+    }
+    else if (n < 70 && n >= 60){
+        cout<<"D"<<endl;
+    }
+    else{
+        cout<<"F"<<endl;
+    }
+}
