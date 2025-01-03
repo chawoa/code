e6266922745ec655c89ec7f0c95a98030be96a58
chawoa <129825032+chawoa@users.noreply.github.com>
2025-01-03 18:37:27 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    short n,num;
+
+    cin>>n;
+
+    num = n / 4;
+
+    for (int i = 0; i < num-1; i++){
+        cout<<"long ";
+    }
+    cout<<"long int"<<endl;
+}
