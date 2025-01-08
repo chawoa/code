92761ce43708e868ea60f11db8c45a5d57ad165b
chawoa <129825032+chawoa@users.noreply.github.com>
2025-01-08 23:23:52 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    short n;
+    string s;
+
+    cin>>s>>n;
+
+    cout<<s[n-1]<<"\n";
+
+}
