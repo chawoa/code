aa96af6fd1ba8cba6ad2ff9a0fbe368fa8dbd9dd
chawoa <129825032+chawoa@users.noreply.github.com>
2025-01-09 00:39:29 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
+
+    char s;
+
+    cin>>s;
+    cout<<static_cast<int>(s)<<"\n";
+
+
+}
